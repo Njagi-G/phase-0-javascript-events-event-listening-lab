1bc6d808c1a62bade3b1c2b0205f69b3c2d9ead4
Njagi Githaiga <njagigithaiga@gmail.com>
2024-04-08 14:05:07 +0300

    Submitting event-listening lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
+    
+    input.addEventListener("click", function () {
+
+        alert ("Good Job! You have passed the test!");
+    }) 
 }
+
+const input = document.getElementById("button");
+
+addingEventListener(input);
+
